d413dafc082599e39e5dc1d0d40f0b699fa6e133
Diana-nyamai <dnyamai.dn@gmail.com>
2022-06-24 20:22:35 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+// import './css/Navbar.css'
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../css/Navbar.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='header'>
+     <img src="./dnt.png" alt='logo'/>
+     <ul>
+        <li>ABOUT</li>
+        <li>PROJECT</li>
+        <li>CERTIFICATES</li>
+        <li>CONTACT</li>
+     </ul>
+    </div>
   )
 }
 
