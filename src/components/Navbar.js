b38cdf930cbec0ee631e6c11cb053ac414f47bc3
Diana-nyamai <dnyamai.dn@gmail.com>
2022-06-27 19:34:12 +0300

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <label for="check" className='checkbtn'><i className="fa fa-bars"></i></label>
 
      <ul>
-        <li><a href='#'>ABOUT</a></li>
+        <li><a href='./about.js'>ABOUT</a></li>
         <li><a href='#'>PROJECT</a></li>
         <li><a href='#'>CERTIFICATES</a></li>
         <li><a href='#'>CONTACT</a></li>
