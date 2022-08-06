72db43b636988dd77d2a589935d3b8a0a07bfb4d
Diana-nyamai <dnyamai.dn@gmail.com>
2022-08-06 12:42:59 +0300

    -about section complete - responsive about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import "../css/About.css";
 
 function About() {
   return (
-    <div className='about' id="about">About
-     <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.
-
-     Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.
-     
-     Versions of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by advertisements for Letraset transfer sheets.[1] Lorem ipsum was introduced to the digital world in the mid-1980s, when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipsum,[2] as have many LaTeX packages,[3][4][5] web content managers such as Joomla! and WordPress, and CSS libraries such as Semantic UI.[6]</p>
+    <div className="about" id="about">
+      <h2>About</h2>
+      <div className="main-contain">
+        <div className="childcontainer1">
+          <img src="../fav.svg" alt="fav" />
+        </div>
+        <div className="childcontainer2">
+          <p>My name is Diana Ndinda Nyamai.</p>
+          <p>I am a diligent and ambitious individual 
+          with a great passion in Front-end 
+          development. </p>
+          <p>I am proficient in the technologies: 
+          Javascript,reactjs, html5,css,
+          php,vuejs and python</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
