48c79447b25a3cabec264d865fffe3dc1afac822
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-07 11:35:06 +0300

    Fix: formatting code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import './App.css';
+import "./App.css";
 import ClipLoader from "react-spinners/CircleLoader";
-import Hero from './components/Hero';
-import { useEffect, useState } from 'react';
+import Hero from "./components/Hero";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(false);
 
-  useEffect(() =>{
-    setLoading(true)
+  useEffect(() => {
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 1000)
-  }, [])
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div className='app'>
-    {
-      loading? 
-      <ClipLoader color={'#E87DA4'} loading={loading} size={120} />
-      :
-      <Hero/>
-    }
-      
+    <div className="app">
+      {loading ? (
+        <ClipLoader color={"#E87DA4"} loading={loading} size={120} />
+      ) : (
+        <Hero />
+      )}
     </div>
   );
 }
