ce30e30b8b11a0ecc907de513d77cc3dd3647a7c
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-14 08:22:06 +0300

    Update: clearing all images & unwanted components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import "./App.css";
-import ClipLoader from "react-spinners/CircleLoader";
-import Hero from "./components/Hero";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
+  
   return (
     <div className="app">
-      {loading ? (
-        <ClipLoader color={"#E87DA4"} loading={loading} size={120} />
-      ) : (
-        <Hero />
-      )}
+      
     </div>
   );
 }
