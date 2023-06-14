5bf6235b45654f3ad4106e93ce9630dad2659c44
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-14 21:58:47 +0300

    Update: Added the navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   
   return (
     <div className="app">
-      
+      <Navbar/>
     </div>
   );
 }
