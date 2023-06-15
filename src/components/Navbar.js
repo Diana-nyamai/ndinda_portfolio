277ba615afb19a0f805dea5c18be8ca2b17241bf
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-15 12:39:17 +0300

    Updated the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
 import React, { useRef } from "react";
 import { FaTimes } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const lists = [
+    {
+      navItem: "Home",
+      navLink: "/",
+    },
+    {
+      navItem: "About",
+      navLink: "/About",
+    },
+    {
+      navItem: "Projects",
+      navLink: "/Projects",
+    },
+    {
+      navItem: "Blogs",
+      navLink: "/Blogs",
+    },
+    {
+      navItem: "contact",
+      navLink: "Contact",
+    },
+  ];
   const navRef = useRef();
 
   const showMenu = () => {
@@ -12,14 +35,19 @@ function Navbar() {
     <header>
       <h4>DIANA NDINDA</h4>
       <nav ref={navRef}>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Blogs</li>
-            <li>Contact</li>
-          <button className="navBtn navCloseBtn" onClick={showMenu}>
-            <FaTimes />
-          </button>
+        {lists.map((list, index) => {
+          return (
+            <li key={index}>
+              <NavLink to={list.navLink} exact onClick={showMenu}>
+                {list.navItem}
+              </NavLink>
+            </li>
+          );
+        })}
+
+        <button className="navBtn navCloseBtn" onClick={showMenu}>
+          <FaTimes />
+        </button>
       </nav>
       <button className="navBtn" onClick={showMenu}>
         <GiHamburgerMenu />
