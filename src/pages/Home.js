9c2aaa68b300d189a3a662b1b9ace41850108b7c
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-15 12:40:09 +0300

    Updated the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import illustration from '../assets/illustration.png'
 
 function Home() {
   return (
-    <div>
-      
+    <div className='homeContainer'>
+     <div className="greetings">
+
+      <div className="homeText">
+        <p>Hello there,</p>
+        <h1>My name is Diana Nyamai</h1>
+        <p>I am a Front-End Engineer</p>
+      </div>
+      <div className="homeBtn">
+        <button>Get in touch</button>
+        <button>Download cv</button>
+      </div>
+     </div>
+
+     <div className="techImg">
+        <img src={illustration} alt="illustration" />
+     </div>
     </div>
   )
 }
