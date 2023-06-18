6873e95b1a4013e9bdb5986b2265b9a4bea71f63
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-18 19:41:31 +0300

    Installing animate css package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import 'animate.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
