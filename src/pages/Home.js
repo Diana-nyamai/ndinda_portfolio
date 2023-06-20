042ef48c559d8f272a1211cda0cb7f668ab10f8f
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-20 09:38:24 +0300

    Update: formatting the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import illustration from '../assets/illustration.png'
-import Button1 from '../components/Button1'
+import React from "react";
+import illustration from "../assets/illustration.png";
+import Button1 from "../components/Button1";
 
 function Home() {
   return (
-    <div className='homeContainer'>
-     <div className="greetings">
-
-      <div className="homeText">
-        <p className="animate__animated animate__backInDown">Hello there,</p>
-        <h1 className=" animate__animated animate__backInLeft">My name is Diana Nyamai</h1>
-        <p className='animate__animated animate__backInRight'>I am a Front-End Engineer</p>
+    <div className="homeContainer">
+      <div className="greetings">
+        <div className="homeText">
+          <p className="animate__animated animate__backInDown">Hello there,</p>
+          <h1 className=" animate__animated animate__backInLeft">
+            My name is Diana Nyamai
+          </h1>
+          <p className="animate__animated animate__backInRight">
+            I am a Front-End Engineer
+          </p>
+        </div>
+        <div className="homeBtn">
+          <Button1 name="Get in Touch" link="/Contact" />
+          <Button1 name="Download CV" link="/About" />
+        </div>
       </div>
-      <div className="homeBtn">
-        <Button1 name="Get in Touch" link="/Contact"/>
-        <Button1 name="Download CV" link="/About"/>
-      </div>
-     </div>
 
-     <div className="techImg">
-        <img className='animate__animated animate__zoomIn' src={illustration} alt="illustration" />
-     </div>
+      <div className="techImg">
+        <img
+          className="animate__animated animate__zoomIn"
+          src={illustration}
+          alt="illustration"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
