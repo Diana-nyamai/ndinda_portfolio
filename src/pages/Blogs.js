e4ce78f95141d818c1c5bc8c5138b678520643a5
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-21 08:57:48 +0300

    Update: adding content to the blog page

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Blogs() {
   return (
-    <div>
-      this is blogs
+    <div className='blogContainer'>
+      <div className="blogCard">
+        <div className="blogBox">
+          <div className="blogContent">
+            <span className='cardHeading'>01</span>
+              <span className='blogContent'>Recursion</span>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt eligendi eius voluptas architecto repudiandae. Maiores accusantium soluta suscipit modi, id tempora culpa? Voluptatum iure ullam aliquid nulla a et molestias.</p>
+              <Link to="#">Read More</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
