631beb8944f1d7a4d551fd113dd4f5375009b3cf
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-27 17:45:31 +0300

    creating and designing the project page

---
+++ src/components/ProjectData.js
@@ -0,0 +1,74 @@
+import pinkpalace from "../assets/projects/pinkpalace.png";
+import js from "../assets/icons/js.png";
+import css from "../assets/icons/css-3.png";
+import html from "../assets/icons/html.png"; 
+
+const data = {
+    lists:[
+        {
+          mainImg: pinkpalace,
+          name: "PinkPalace",
+          description: "A web application that allows users to create and manage their own pink palace.",
+          technologies: [
+            {
+              name: "HTML",
+              icon: html,
+            },
+            {
+              name: "CSS",
+              icon: css,
+            },
+            {
+              name: "JavaScript",
+              icon: js,
+            },
+          ],
+          liveLink: "https://pinkpalace.co.ke/",
+          codeLink: "https://github.com/Diana-nyamai/PINK-PALACE",
+        },
+        {
+            mainImg: pinkpalace,
+            name: "PinkPalace",
+            description: "A web application that allows users to create and manage their own pink palace.",
+            technologies: [
+              {
+                name: "HTML",
+                icon: html,
+              },
+              {
+                name: "CSS",
+                icon: css,
+              },
+              {
+                name: "JavaScript",
+                icon: js,
+              },
+            ],
+            liveLink: "https://pinkpalace.herokuapp.com/",
+            codeLink: "https://github.com/Jonathan-Harris/pinkpalace",
+          },
+          {
+            mainImg: pinkpalace,
+            name: "PinkPalace",
+            description: "A web application that allows users to create and manage their own pink palace.",
+            technologies: [
+              {
+                name: "HTML",
+                icon: html,
+              },
+              {
+                name: "CSS",
+                icon: css,
+              },
+              {
+                name: "JavaScript",
+                icon: js,
+              },
+            ],
+            liveLink: "https://pinkpalace.herokuapp.com/",
+            codeLink: "https://github.com/Jonathan-Harris/pinkpalace",
+          }
+      ]
+}
+
+export default data;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import data from "../components/ProjectData";
+import Button3 from "../components/Button3";
+import {FiExternalLink} from "react-icons/fi"
+import {BsGithub} from "react-icons/bs"
 
 function Projects() {
   return (
-    <div>
-      this is projects
+    <div className="mainProjectContainer">
+      <h1 className="animate__animated animate__backInDown">My Projects</h1>
+      {data.lists.map((list, index) => (
+        <div
+          key={index}
+          className="projectContainer animate__animated animate__zoomIn"
+        >
+          <div className="projectBox">
+            <div className="projectPrototype">
+              <img src={list.mainImg} alt="projects" />
+            </div>
+            <div className="projectInfo">
+              <div className="projectContent">
+                <h3>{list.name}</h3>
+                <p>{list.description}</p>
+              </div>
+              <div className="projectIcons">
+                <p>Technologies used:</p>
+                <div className="languageList">
+                  {" "}
+                  {list.technologies.map((item, index) => (
+                    <li key={index}>
+                      <span>
+                        <img src={item.icon} alt="icons" />
+                      </span>
+                      {item.name}
+                    </li>
+                  ))}
+                </div>
+              </div>
+              <div className="projectButtons">
+                <Button3 name="View Live" icon=<FiExternalLink/> link ={list.liveLink}/>
+                <Button3 name="View Code" icon=<BsGithub/> link ={list.codeLink}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
