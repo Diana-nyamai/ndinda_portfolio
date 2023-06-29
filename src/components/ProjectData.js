2fa8ffe87191605291ace579ad147061dc2437bc
Diana-nyamai <dnyamai.dn@gmail.com>
2023-06-29 13:49:29 +0300

    Update: adding the project data

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,14 +1,17 @@
 import pinkpalace from "../assets/projects/pinkpalace.png";
+import executive from "../assets/projects/executive.png";
+import chatapp from "../assets/projects/chatapp.png";
 import js from "../assets/icons/js.png";
 import css from "../assets/icons/css-3.png";
 import html from "../assets/icons/html.png"; 
+import react from "../assets/icons/react.png"
 
 const data = {
     lists:[
         {
           mainImg: pinkpalace,
           name: "PinkPalace",
-          description: "A web application that allows users to create and manage their own pink palace.",
+          description: "Pink Palace is a frontend project aimed at creating a website that provides information about a beautiful location where users can book photography grounds, host wedding parties, and celebrate birthdays.",
           technologies: [
             {
               name: "HTML",
@@ -27,31 +30,35 @@ const data = {
           codeLink: "https://github.com/Diana-nyamai/PINK-PALACE",
         },
         {
-            mainImg: pinkpalace,
-            name: "PinkPalace",
-            description: "A web application that allows users to create and manage their own pink palace.",
+            mainImg: executive,
+            name: "Executive Matchmaking",
+            description: "This is a web application developed utilizing Reactjs, and CSS to cater to individuals searching for romantic relationships, with a focus on those seeking a more targeted approach to finding compatible partners",
             technologies: [
               {
-                name: "HTML",
-                icon: html,
+                name: "REACTJS",
+                icon: react,
               },
               {
                 name: "CSS",
                 icon: css,
               },
               {
-                name: "JavaScript",
-                icon: js,
+                name: "HTML",
+                icon: html,
               },
             ],
-            liveLink: "https://pinkpalace.herokuapp.com/",
-            codeLink: "https://github.com/Jonathan-Harris/pinkpalace",
+            liveLink: "https://executivematchmaking.netlify.app/",
+            codeLink: "https://github.com/Diana-nyamai/Executive-matchmaking",
           },
           {
-            mainImg: pinkpalace,
-            name: "PinkPalace",
-            description: "A web application that allows users to create and manage their own pink palace.",
+            mainImg: chatapp,
+            name: "Chat Application",
+            description: "The Chat application is a web-based application that emulates the core features and functionality of the popular Social media application, Facebook.",
             technologies: [
+              {
+                name: "REACTJS",
+                icon: react,
+              },
               {
                 name: "HTML",
                 icon: html,
@@ -60,13 +67,9 @@ const data = {
                 name: "CSS",
                 icon: css,
               },
-              {
-                name: "JavaScript",
-                icon: js,
-              },
             ],
-            liveLink: "https://pinkpalace.herokuapp.com/",
-            codeLink: "https://github.com/Jonathan-Harris/pinkpalace",
+            liveLink: "https://diana-nyamai.github.io/Chat-application/",
+            codeLink: "https://github.com/Diana-nyamai/Chat-application",
           }
       ]
 }
